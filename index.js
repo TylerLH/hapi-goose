3c9058c9aa3cde23618d3653ef6aedd906eef4f7
Tyler Hughes <iampbt@gmail.com>
2014-11-17 19:49:16 -0800

    fixing some bugs

--- index.js
+++ index.js
@@ -5,27 +5,26 @@
 
 var mongoose = require('mongoose');
 
-// Connect to database
-function connect(cb) {
-  plugin.log(['info', 'mongoose'], 'Connecting to database: ' + options.mongodb_uri);
-  mongoose.connect(function(err) {
-    if (err) {
-      plugin.log(['info', 'mongoose'], 'Error connecting to database: ' + options.mongodb_uri);
-      cb(err);
-    } else {
-      plugin.log(['info', 'mongoose'], 'Connected to database: ' + options.mongodb_uri);
-      cb();
-    };
-  })
-};
-
-// Disconnect from database
-function disconnect() {
-  mongoose.disconnect();
-};
-
 exports.register = function(plugin, options, next) {
-  var db = mongoose.connect(options.mongodb_uri);
+  // Connect to database
+  function connect(cb) {
+    plugin.log(['info', 'mongoose'], 'Connecting to database: ' + options.mongodb_uri);
+    mongoose.connect(options.mongodb_uri, function(err) {
+      if (err) {
+        plugin.log(['info', 'mongoose'], 'Error connecting to database: ' + options.mongodb_uri);
+        cb(err);
+      } else {
+        plugin.log(['info', 'mongoose'], 'Connected to database: ' + options.mongodb_uri);
+        cb();
+      };
+    })
+  };
+
+  // Disconnect from database
+  function disconnect() {
+    mongoose.disconnect();
+  };
+
   plugin.expose('instance', mongoose);
   plugin.expose('connect', connect);
   plugin.expose('disconnect', disconnect);
